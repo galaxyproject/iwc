efca5af98a319e89959e01ca0ae70244f5ef8809
Dannon Baker <dannon.baker@gmail.com>
2024-09-10 08:24:40 -0400

    use marked, parse markdown, render readme/changelog

--- website/pages/workflow/[id].vue
+++ website/pages/workflow/[id].vue
@@ -3,6 +3,7 @@ import { ref, computed } from 'vue'
 import { useRoute } from 'vue-router'
 import { type Workflow, type WorkflowCollection } from '~/models/workflow'
 import { workflowCollections } from '~/models/workflow'
+import { marked } from 'marked'
 
 const route = useRoute()
 const workflow = ref<Workflow | null>(null)
@@ -20,36 +21,33 @@ const formatDate = (date: string) => {
     day: 'numeric'
   })
 }
+
+const parseMarkdown = (content: string) => {
+  return marked(content)
+}
 </script>
 
 <template>
   <div v-if="workflow" class="max-w-3xl mx-auto py-8">
     <h1 class="text-3xl font-bold mb-4">{{ workflow.name }}</h1>
-    <div class="bg-gray-100 p-4 rounded-lg mb-6">
-      <p class="text-gray-700">{{ workflow.definition.annotation }}</p>
-    </div>
-    <div class="grid grid-cols-2 gap-4 mb-6">
-      <div>
-        <h2 class="text-xl font-semibold mb-2">Details</h2>
-        <p><strong>Authors:</strong> {{ workflow.authors.join(', ') }}</p>
-        <p><strong>Release:</strong> {{ workflow.definition.release}}</p>
+    <div class="bg-gray-100 p-6 rounded-lg mb-6 text-gray-800">
+      <p class="mb-4">{{ workflow.definition.annotation }}</p>
+      <h2 class="text-xl font-semibold mb-2">Details</h2>
+      <p><strong>Authors:</strong> {{ workflow.authors.join(', ') }}</p>
+      <p><strong>Release:</strong> {{ workflow.definition.release }}</p>
+      <!-- <h2 class="text-xl font-semibold mt-4 mb-2">Categories</h2>
+      <ul class="list-disc list-inside">
+        <li v-for="category in workflow.definition.categories" :key="category">
+          {{ category }}
+        </li>
+      </ul> -->
+      <div v-if="workflow.readme" class="mt-6">
+        <div class="prose prose-sm" v-html="parseMarkdown(workflow.readme)"></div>
       </div>
-      <!-- <div>
-        <h2 class="text-xl font-semibold mb-2">Categories</h2>
-        <ul class="list-disc list-inside">
-          <li v-for="category in workflow.definition.categories" :key="category">
-            {{ category }}
-          </li>
-        </ul>
-      </div> -->
-    </div>
-    <div v-if="workflow.readme" class="mb-6">
-      <h2 class="text-2xl font-semibold mb-4">README</h2>
-      <div class="prose" v-html="workflow.readme"></div>
-    </div>
-    <div v-if="workflow.changelog" class="mb-6">
-      <h2 class="text-2xl font-semibold mb-4">CHANGELOG</h2>
-      <div class="prose" v-html="workflow.changelog"></div>
+      <div v-if="workflow.changelog" class="mt-6">
+        <h2 class="text-2xl font-semibold mb-4">CHANGELOG</h2>
+        <div class="prose prose-sm" v-html="parseMarkdown(workflow.changelog)"></div>
+      </div>
     </div>
   </div>
   <div v-else class="max-w-3xl mx-auto py-8">
