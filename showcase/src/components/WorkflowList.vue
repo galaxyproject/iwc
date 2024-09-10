e32cbfcfddce07d1beac2199cfd0089acc78bed1
Dannon Baker <dannon.baker@gmail.com>
2024-09-10 08:24:39 -0400

    Incremental work, rebase flat

--- showcase/src/components/WorkflowList.vue
+++ showcase/src/components/WorkflowList.vue
@@ -1,15 +1,28 @@
 <template>
-    <ul>
-      <li v-for="workflow in workflows" :key="workflow.path">
-        <a :href="`/workflow/${workflow.path}`">{{ workflow.workflows[0].name }}</a>
-      </li>
-    </ul>
-  </template>
-  
-  <script>
-  export default {
-    props: {
-      workflows: Array
-    }
+  <h2>Workflows</h2>
+  <div class="grid-container">
+    <div class="grid-item" v-for="workflow in workflows" :key="workflow.path">
+      <a :href="`/workflow/${workflow.path}`">{{ workflow.workflows[0].name }}</a>
+    </div>
+  </div>
+</template>
+
+<style scoped>
+.grid-container {
+  display: grid;
+  grid-template-columns: repeat(3, 1fr);
+  gap: 10px;
+}
+</style>
+
+<script>
+export default {
+  props: {
+    workflows: Array
+  },
+  mounted() {
+    console.debug("hello");
+    console.debug(this.workflows);
   }
-  </script>
+}
+</script>
