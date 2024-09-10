a47187dfc21decaf70d8b8f75eb81b83e3a5977c
Dannon Baker <dannon.baker@gmail.com>
2024-09-10 08:24:39 -0400

    basic astro

---
+++ showcase/src/components/WorkflowList.vue
@@ -0,0 +1,15 @@
+<template>
+    <ul>
+      <li v-for="workflow in workflows" :key="workflow.path">
+        <a :href="`/workflow/${workflow.path}`">{{ workflow.workflows[0].name }}</a>
+      </li>
+    </ul>
+  </template>
+  
+  <script>
+  export default {
+    props: {
+      workflows: Array
+    }
+  }
+  </script>
