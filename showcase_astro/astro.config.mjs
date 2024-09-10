1ca1e43a65cf351d9c19e0e0821ba5e0138fd8a9
Dannon Baker <dannon.baker@gmail.com>
2024-09-10 08:24:40 -0400

    Drop astro

--- showcase_astro/astro.config.mjs
+++
@@ -1,9 +0,0 @@
-import { defineConfig } from 'astro/config';
-import vue from "@astrojs/vue";
-
-import tailwind from "@astrojs/tailwind";
-
-// https://astro.build/config
-export default defineConfig({
-  integrations: [vue(), tailwind()]
-});
