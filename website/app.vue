202c93359e99e15e7b887e6bd0e930ee9cd666d4
Dannon Baker <dannon.baker@gmail.com>
2024-09-10 08:24:40 -0400

    Simpler styling for card outline

--- website/app.vue
+++ website/app.vue
@@ -1,14 +1,14 @@
 <template>
-  <div>
-    <UContainer>
-      <UCard class="mt-10">
-        <template #header>
-          <div class="flex justify-between">
-            <h1>Galaxy Workflow Library</h1>
-          </div>
-        </template>
-        <NuxtPage />
-      </UCard>
+  <div class="min-h-screen bg-gray-50">
+    <header class="bg-white shadow-sm">
+      <UContainer class="py-4">
+        <div class="flex justify-between items-center">
+          <h1 class="text-xl font-semibold text-gray-800">Galaxy Workflow Library</h1>
+        </div>
+      </UContainer>
+    </header>
+    <UContainer class="py-8">
+      <NuxtPage />
     </UContainer>
   </div>
 </template>
