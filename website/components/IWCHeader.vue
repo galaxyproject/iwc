d4b7e9523ddb15e05b187fc0e370d6d1aa731fa4
Dannon Baker <dannon.baker@gmail.com>
2024-09-10 10:41:30 -0400

    temporary logo...

--- website/components/IWCHeader.vue
+++ website/components/IWCHeader.vue
@@ -1,9 +1,12 @@
 <template>
-    <header>
-        <UContainer class="py-8">
-            <div>
-                <NuxtLink to="/" class="text-xl font-semibold">
-                    Galaxy IWC - Workflow Library (todo: add a fancy banner)
+    <header class="shadow-md">
+        <UContainer class="py-4">
+            <div class="flex items-center justify-between">
+                <NuxtLink to="/" class="flex items-center space-x-2">
+                    <div class="w-10 h-10 bg-primary-500 rounded-full flex items-center justify-center">
+                        <span class="text-white text-xl font-bold">IWC</span>
+                    </div>
+                    <span class="text-xl font-semibold">Galaxy IWC - Workflow Library</span>
                 </NuxtLink>
             </div>
         </UContainer>
