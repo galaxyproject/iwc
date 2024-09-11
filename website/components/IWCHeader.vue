ca9068223cb1c6dcf3ebd0831e1595241e4e5e8a
Dannon Baker <dannon.baker@gmail.com>
2024-09-11 11:39:25 -0400

    Header tweaks, use logo -- not sure if it's bold enough?

--- website/components/IWCHeader.vue
+++ website/components/IWCHeader.vue
@@ -1,19 +1,17 @@
 <template>
     <header class="shadow-md">
         <UContainer class="py-4">
-            <div class="flex items-center justify-between">
-                <div class="flex items-center space-x-2">
+            <div class="flex">
+                <div class="flex flex-grow space-x-2">
                     <NuxtLink to="/" class="flex items-center space-x-2">
-                        <div class="w-10 h-10 bg-primary-500 rounded-full flex items-center justify-center">
-                            <span class="text-white text-xl font-bold">IWC</span>
-                        </div>
+                        <img src="/iwc_logo.png" alt="IWC Logo" width="64" height="64"/>
                         <span class="text-xl font-semibold">Galaxy IWC - Workflow Library</span>
                     </NuxtLink>
                 </div>
                 <div class="flex items-center space-x-2">
-                    <NuxtLink to="/about" class="text-gray-600 hover:text-primary">About</NuxtLink>
-                    <NuxtLink to="/docs" class="text-gray-600 hover:text-primary">Docs</NuxtLink>
-                    <NuxtLink to="/help" class="text-gray-600 hover:text-primary">Help</NuxtLink>
+                    <NuxtLink to="/about">About</NuxtLink>
+                    <NuxtLink to="https://planemo.readthedocs.io/en/latest/best_practices_workflows.html">Docs</NuxtLink>
+                    <NuxtLink to="https://github.com/galaxyproject/iwc/">Help</NuxtLink>
                 </div>
             </div>
         </UContainer>
