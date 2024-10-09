6d128b419c071bd43a3a28b7ca8386ad47a38867
fubar2 <ross.lazarus@gmail.com>
2024-10-10 09:49:47 +1100

    both EBPhisb workflows pass tests at least...

--- workflows/VGP-assembly-v2/EBPhisb-dimer-coverage-SV/tests.sh
+++
@@ -1,2 +0,0 @@
-# planemo workflow_test_init --from_invocation 17d1f86a00c3056e --galaxy_url https://vgp.usegalaxy.org --galaxy_user_key 9f7bdc0d74965f1a443c01aac2d6956d
-planemo test --galaxy_url https://vgp.usegalaxy.org --galaxy_user_key 9f7bdc0d74965f1a443c01aac2d6956d dimer-density-SV-and-coverage.ga 
