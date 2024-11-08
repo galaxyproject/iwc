72f83147b66369630ae8039f24f23a5ae320a668
Dannon Baker <dannon.baker@gmail.com>
2024-11-08 12:42:16 -0500

    App/nuxt config updates for styling -- very much a WIP

--- website/app.config.ts
+++ website/app.config.ts
@@ -1,6 +1,8 @@
 export default defineAppConfig({
     ui: {
-        primary: "amber",
-        gray: "neutral",
+        // primary: "amber",
+        // This is a lot... amber is better?  Keep iterating towards galaxy-ish.
+        primary: "gold",
+        gray: "ebony-clay",
     },
 });
--- website/nuxt.config.ts
+++ website/nuxt.config.ts
@@ -1,9 +1,8 @@
 import { defineNuxtConfig } from "nuxt/config";
-import path from "path";
-import { promises as fs } from "fs";
 
-// https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-    devtools: { enabled: true },
     modules: ["@nuxt/ui", "@nuxt/content", "@pinia/nuxt"],
+    colorMode: {
+        preference: 'light'
+    },
 });
