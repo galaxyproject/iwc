4ebb200541ce74e7124b4362f2f8bb3cd4a5bedd
Dannon Baker <dannon.baker@gmail.com>
2024-11-08 17:16:09 -0500

    Header space/padding tweaks

--- website/components/IWCHeader.vue
+++ website/components/IWCHeader.vue
@@ -1,5 +1,5 @@
 <template>
-    <header class="p-16 bg-ebony-clay-800">
+    <header class="p-8 bg-ebony-clay-800">
         <div class="flex">
             <div class="flex flex-grow space-x-2">
                 <NuxtLink to="/" class="flex items-center space-x-2">
@@ -9,7 +9,7 @@
                     </span>
                 </NuxtLink>
             </div>
-            <div class="flex items-center space-x-8 px-12 font-mono text-chicago-50">
+            <div class="flex items-center space-x-8 font-mono text-chicago-50">
                 <NuxtLink to="https://github.com/galaxyproject/iwc/blob/main/workflows/README.md#adding-workflows">
                     Contribute
                 </NuxtLink>
