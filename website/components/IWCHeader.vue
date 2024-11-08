0aed1dc1c33925bf198d66a975cfd1d4186c46c4
Dannon Baker <dannon.baker@gmail.com>
2024-11-08 12:39:39 -0500

    Header restructuring

--- website/components/IWCHeader.vue
+++ website/components/IWCHeader.vue
@@ -1,18 +1,18 @@
 <template>
-    <header class="shadow-md">
-        <UContainer class="py-4">
-            <div class="flex">
-                <div class="flex flex-grow space-x-2">
-                    <NuxtLink to="/" class="flex items-center space-x-2">
-                        <img src="/iwc_logo.png" alt="IWC Logo" width="64" height="64" />
-                        <span class="text-xl font-semibold">Galaxy IWC - Workflow Library</span>
-                    </NuxtLink>
-                </div>
-                <div class="flex items-center space-x-2">
-                    <NuxtLink to="https://github.com/galaxyproject/iwc/blob/main/workflows/README.md#adding-workflows">Contribute</NuxtLink>
-                    <NuxtLink to="https://github.com/galaxyproject/iwc/">GitHub</NuxtLink>
-                </div>
+    <header class="p-16">
+        <div class="flex">
+            <div class="flex flex-grow space-x-2">
+                <NuxtLink to="/" class="flex items-center space-x-2">
+                    <img src="/iwc_logo.png" alt="IWC Logo" width="64" height="64" />
+                    <span class="text-xl font-semibold">Galaxy IWC - Workflow Library</span>
+                </NuxtLink>
             </div>
-        </UContainer>
+            <div class="flex items-center space-x-8 px-12">
+                <NuxtLink to="https://github.com/galaxyproject/iwc/blob/main/workflows/README.md#adding-workflows">
+                    Contribute
+                </NuxtLink>
+                <NuxtLink to="https://github.com/galaxyproject/iwc/">GitHub</NuxtLink>
+            </div>
+        </div>
     </header>
 </template>
