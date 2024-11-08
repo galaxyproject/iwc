3efa0e4cb36ef2f1d5d57c46bc2718d4401e8844
Dannon Baker <dannon.baker@gmail.com>
2024-11-08 14:16:22 -0500

    Drop debug text

--- website/layouts/default.vue
+++ website/layouts/default.vue
@@ -2,13 +2,11 @@
     <div class="flex w-full">
         <!-- Left sidebar -->
         <div class="w-1/4 p-4 overflow-y-auto">
-            <h1>sidebar</h1>
             <slot name="sidebar"></slot>
         </div>
 
         <!-- Right side content -->
         <div class="w-3/4 p-4 overflow-y-auto">
-            <h1>content</h1>
             <slot name="content"></slot>
         </div>
     </div>
