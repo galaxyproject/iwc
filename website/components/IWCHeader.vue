70ff855444e41f28ea7ac3f02fa62f13d6ab24fc
Dannon Baker <dannon.baker@gmail.com>
2024-11-08 14:10:15 -0500

    Header updates, customized logo w/ white

--- website/components/IWCHeader.vue
+++ website/components/IWCHeader.vue
@@ -1,13 +1,15 @@
 <template>
-    <header class="p-16">
+    <header class="p-16 bg-ebony-clay-800">
         <div class="flex">
             <div class="flex flex-grow space-x-2">
                 <NuxtLink to="/" class="flex items-center space-x-2">
-                    <img src="/iwc_logo.png" alt="IWC Logo" width="64" height="64" />
-                    <span class="text-xl font-semibold">Galaxy IWC - Workflow Library</span>
+                    <img src="/iwc_logo_white.png" alt="IWC Logo" width="128" height="128" />
+                    <span class="text-4xl font-mono font-semibold text-chicago-50 px-4">
+                        Galaxy IWC - Workflow Library
+                    </span>
                 </NuxtLink>
             </div>
-            <div class="flex items-center space-x-8 px-12">
+            <div class="flex items-center space-x-8 px-12 font-mono text-chicago-50">
                 <NuxtLink to="https://github.com/galaxyproject/iwc/blob/main/workflows/README.md#adding-workflows">
                     Contribute
                 </NuxtLink>
