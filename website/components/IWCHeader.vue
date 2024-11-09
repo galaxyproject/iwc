c6f7f9d4b4e38b027f3e9e4d99e3d78ac6ddedf0
Dannon Baker <dannon.baker@gmail.com>
2024-11-08 20:14:08 -0500

    Add link hovers to header

--- website/components/IWCHeader.vue
+++ website/components/IWCHeader.vue
@@ -2,18 +2,20 @@
     <header class="p-8 bg-ebony-clay-800 border-b">
         <div class="flex">
             <div class="flex flex-grow space-x-2">
-                <NuxtLink to="/" class="flex items-center space-x-2">
+                <NuxtLink to="/" class="flex items-center space-x-2 hover:text-hokey-pokey">
                     <img src="/iwc_logo_white.png" alt="IWC Logo" width="128" height="128" />
-                    <span class="text-4xl font-mono font-semibold text-chicago-50 px-4">
-                        Galaxy IWC - Workflow Library
-                    </span>
+                    <span class="text-4xl font-mono font-semibold px-4"> Galaxy IWC - Workflow Library </span>
                 </NuxtLink>
             </div>
-            <div class="flex items-center space-x-8 font-mono text-chicago-50">
-                <NuxtLink to="https://github.com/galaxyproject/iwc/blob/main/workflows/README.md#adding-workflows">
+            <div class="flex items-center space-x-8 font-mono">
+                <NuxtLink
+                    to="https://github.com/galaxyproject/iwc/blob/main/workflows/README.md#adding-workflows"
+                    class="hover:text-hokey-pokey">
                     Contribute
                 </NuxtLink>
-                <NuxtLink to="https://github.com/galaxyproject/iwc/">GitHub <Icon name="uil:github" /></NuxtLink>
+                <NuxtLink to="https://github.com/galaxyproject/iwc/" class="hover:text-hokey-pokey">
+                    GitHub <Icon name="uil:github" />
+                </NuxtLink>
             </div>
         </div>
     </header>
