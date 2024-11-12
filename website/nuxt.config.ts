1bbe1f033e33b5878943c32603702ea69fa6479a
Dannon Baker <dannon.baker@gmail.com>
2024-11-11 19:39:58 -0500

    Focus on light mode for now; dark just isn't as nice anymore.  Force white header brand text w/ galaxy dark blue background.

--- website/nuxt.config.ts
+++ website/nuxt.config.ts
@@ -2,4 +2,7 @@ import { defineNuxtConfig } from "nuxt/config";
 
 export default defineNuxtConfig({
     modules: ["@nuxt/ui", "@nuxt/content", "@pinia/nuxt"],
+    colorMode: {
+        preference: "light",
+    },
 });
