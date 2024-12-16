5c44deae995e002a3ab12397e16933e6fd6ffbd7
Dannon Baker <dannon.baker@gmail.com>
2024-12-16 10:03:39 -0500

    Swap workflow listing to grid-3

--- website/pages/index.vue
+++ website/pages/index.vue
@@ -54,36 +54,38 @@ function selectWorkflow(workflow: Workflow) {
             </ul>
         </template>
         <template #content>
-            <UCard
-                v-for="workflow in filteredWorkflows"
-                :key="workflow.definition.uuid"
-                :id="`workflow-${workflow.definition.uuid}`"
-                :ui="{
-                    strategy: 'override',
-                    footer: {
-                        padding: 'px-6 py-2',
-                    },
-                }"
-                class="mb-6">
-                <template #header>
-                    <h2 class="text-xl font-bold mb-2">{{ workflow.definition.name }}</h2>
-                </template>
-                <p class="mb-4">{{ workflow.definition.annotation }}</p>
+            <div class="grid grid-cols-3 gap-4">
+                <UCard
+                    v-for="workflow in filteredWorkflows"
+                    :key="workflow.definition.uuid"
+                    :id="`workflow-${workflow.definition.uuid}`"
+                    :ui="{
+                        strategy: 'override',
+                        footer: {
+                            padding: 'px-6 py-2',
+                        },
+                    }"
+                    class="mb-6">
+                    <template #header>
+                        <h2 class="text-xl font-bold mb-2">{{ workflow.definition.name }}</h2>
+                    </template>
+                    <p class="mb-4">{{ workflow.definition.annotation }}</p>
 
-                <div v-if="workflow.definition.tags && workflow.definition.tags.length > 0" class="mb-4">
-                    <UBadge v-for="tag in workflow.definition.tags" :key="tag" class="mr-2 mb-2" variant="soft">
-                        {{ tag }}
-                    </UBadge>
-                </div>
-                <template #footer>
-                    <div class="flex space-x-4">
-                        <p class="text-xs text-gray-500">Release {{ workflow.definition.release }}</p>
-                        <p class="text-xs text-gray-500">(todo:last_update)</p>
+                    <div v-if="workflow.definition.tags && workflow.definition.tags.length > 0" class="mb-4">
+                        <UBadge v-for="tag in workflow.definition.tags" :key="tag" class="mr-2 mb-2" variant="soft">
+                            {{ tag }}
+                        </UBadge>
                     </div>
-                </template>
+                    <template #footer>
+                        <div class="flex space-x-4">
+                            <p class="text-xs text-gray-500">Release {{ workflow.definition.release }}</p>
+                            <p class="text-xs text-gray-500">(todo:last_update)</p>
+                        </div>
+                    </template>
 
-                <UButton :to="`/workflow/${encodeURIComponent(workflow.trsID)}/`">Details</UButton>
-            </UCard>
+                    <UButton :to="`/workflow/${encodeURIComponent(workflow.trsID)}/`">Details</UButton>
+                </UCard>
+            </div>
         </template>
     </NuxtLayout>
 </template>
