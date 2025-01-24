03fb9ea462eb371baa509ee8a87021cfe07d33b1
Dannon Baker <dannon.baker@gmail.com>
2025-01-24 10:58:56 -0500

    Add compat date to nuxt config

--- website/nuxt.config.ts
+++ website/nuxt.config.ts
@@ -2,12 +2,16 @@ import { defineNuxtConfig } from "nuxt/config";
 
 export default defineNuxtConfig({
     modules: ["@nuxt/ui", "@nuxt/content", "@pinia/nuxt", "@nuxt/icon"],
+
     colorMode: {
         preference: "light",
     },
+
     app: {
         head: {
             link: [{ rel: "icon", type: "image/png", href: "/iwc_logo_white.png" }],
         },
     },
+
+    compatibilityDate: "2025-01-23",
 });
