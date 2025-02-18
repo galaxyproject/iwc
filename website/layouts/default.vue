fed741b41ad19c3b051abe73de96b9fdf60a89aa
Dannon Baker <dannon.baker@gmail.com>
2025-02-17 23:21:01 -0500

    Adjust layout classes to fix unintentional section scroll on homepage.

--- website/layouts/default.vue
+++ website/layouts/default.vue
@@ -1,7 +1,7 @@
 <script setup lang="ts">
-import { computed, useSlots } from "vue";
+import { computed, useSlots, type Slots } from "vue";
 
-const slots = useSlots();
+const slots: Slots = useSlots();
 
 const hasHero = computed(() => {
     return !!slots.hero;
@@ -15,16 +15,16 @@ const hasRightSidebar = computed(() => {
 });
 </script>
 <template>
-    <div :class="hasHero ? 'overflow-y-auto w-full' : 'overflow-hidden w-full'">
+    <div class="w-full" :class="hasHero ? 'overflow-y-auto' : 'overflow-hidden'">
         <!-- Hero -->
         <slot name="hero"></slot>
-        <div class="flex w-full h-full">
+        <div class="flex">
             <!-- Left sidebar -->
             <div v-if="hasLeftSidebar" class="w-1/4 p-4 overflow-y-auto">
                 <slot name="leftSidebar"></slot>
             </div>
             <!-- content -->
-            <div class="flex-1 p-4 overflow-y-auto">
+            <div class="flex-1 p-4" :class="{ 'overflow-y-auto': !hasHero }">
                 <slot name="content"></slot>
             </div>
             <!-- Right sidebar -->
