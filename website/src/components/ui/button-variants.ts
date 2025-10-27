ca9c35ecb153e63b563eb70be4a54d150eb3ac08
Dannon Baker <dannon.baker@gmail.com>
2025-10-27 09:12:08 -0400

    Add cursor-pointer to button variants

--- website/src/components/ui/button-variants.ts
+++ website/src/components/ui/button-variants.ts
@@ -1,7 +1,7 @@
 import { type VariantProps, cva } from "class-variance-authority";
 
 export const buttonVariants = cva(
-    "inline-flex items-center justify-center gap-2 whitespace-nowrap rounded-md text-sm font-medium transition-colors focus-visible:outline-none focus-visible:ring-1 focus-visible:ring-gray-950 disabled:pointer-events-none disabled:opacity-50",
+    "inline-flex items-center justify-center gap-2 whitespace-nowrap rounded-md text-sm font-medium transition-colors focus-visible:outline-none focus-visible:ring-1 focus-visible:ring-gray-950 disabled:pointer-events-none disabled:opacity-50 cursor-pointer",
     {
         variants: {
             variant: {
