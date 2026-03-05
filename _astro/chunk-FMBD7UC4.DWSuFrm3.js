344a139e340b1d750e8aa23ac805bf6df8ac394e
bgruening <bgruening@users.noreply.github.com>
2026-03-05 06:26:50 +0000

    deploy: aa1e05c72f89beebff7ca0ce406e8680225046b6

---
+++ _astro/chunk-FMBD7UC4.DWSuFrm3.js
@@ -0,0 +1,15 @@
+import{_ as o}from"./MarkdownRenderer.CuioInqV.js";(function(){try{var e=typeof window<"u"?window:typeof global<"u"?global:typeof globalThis<"u"?globalThis:typeof self<"u"?self:{};e.SENTRY_RELEASE={id:"aa1e05c72f89beebff7ca0ce406e8680225046b6"};var n=new e.Error().stack;n&&(e._sentryDebugIds=e._sentryDebugIds||{},e._sentryDebugIds[n]="ca9e2d3e-81eb-4523-9a94-8e4b7821d8ef",e._sentryDebugIdIdentifier="sentry-dbid-ca9e2d3e-81eb-4523-9a94-8e4b7821d8ef")}catch{}})();var l=o(()=>`
+  /* Font Awesome icon styling - consolidated */
+  .label-icon {
+    display: inline-block;
+    height: 1em;
+    overflow: visible;
+    vertical-align: -0.125em;
+  }
+  
+  .node .label-icon path {
+    fill: currentColor;
+    stroke: revert;
+    stroke-width: revert;
+  }
+`,"getIconStyles");export{l as g};
