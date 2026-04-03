8ee90e5f1ff3f23ae6eb4e1d5a6107d1dd5b6c8f
mvdbeek <mvdbeek@users.noreply.github.com>
2026-04-03 07:43:55 +0000

    deploy: 022a09e2244d5e3185b54053216772f58852c9bb

---
+++ _astro/createLucideIcon.CT-mPv7N.js
@@ -0,0 +1,21 @@
+import{z as c}from"./runtime-core.esm-bundler.Blv9O8p0.js";(function(){try{var e=typeof window<"u"?window:typeof global<"u"?global:typeof globalThis<"u"?globalThis:typeof self<"u"?self:{};e.SENTRY_RELEASE={id:"022a09e2244d5e3185b54053216772f58852c9bb"};var t=new e.Error().stack;t&&(e._sentryDebugIds=e._sentryDebugIds||{},e._sentryDebugIds[t]="7ebbe703-7ce1-4707-ac05-77021efeea34",e._sentryDebugIdIdentifier="sentry-dbid-7ebbe703-7ce1-4707-ac05-77021efeea34")}catch{}})();/**
+ * @license lucide-vue-next v0.562.0 - ISC
+ *
+ * This source code is licensed under the ISC license.
+ * See the LICENSE file in the root directory of this source tree.
+ */const u=e=>e.replace(/([a-z0-9])([A-Z])/g,"$1-$2").toLowerCase(),b=e=>e.replace(/^([A-Z])|[\s-_]+(\w)/g,(t,r,o)=>o?o.toUpperCase():r.toLowerCase()),h=e=>{const t=b(e);return t.charAt(0).toUpperCase()+t.slice(1)},p=(...e)=>e.filter((t,r,o)=>!!t&&t.trim()!==""&&o.indexOf(t)===r).join(" ").trim(),f=e=>e==="";/**
+ * @license lucide-vue-next v0.562.0 - ISC
+ *
+ * This source code is licensed under the ISC license.
+ * See the LICENSE file in the root directory of this source tree.
+ */var n={xmlns:"http://www.w3.org/2000/svg",width:24,height:24,viewBox:"0 0 24 24",fill:"none",stroke:"currentColor","stroke-width":2,"stroke-linecap":"round","stroke-linejoin":"round"};/**
+ * @license lucide-vue-next v0.562.0 - ISC
+ *
+ * This source code is licensed under the ISC license.
+ * See the LICENSE file in the root directory of this source tree.
+ */const y=({name:e,iconNode:t,absoluteStrokeWidth:r,"absolute-stroke-width":o,strokeWidth:s,"stroke-width":d,size:i=n.width,color:w=n.stroke,...a},{slots:l})=>c("svg",{...n,...a,width:i,height:i,stroke:w,"stroke-width":f(r)||f(o)||r===!0||o===!0?Number(s||d||n["stroke-width"])*24/Number(i):s||d||n["stroke-width"],class:p("lucide",a.class,...e?[`lucide-${u(h(e))}-icon`,`lucide-${u(e)}`]:["lucide-icon"])},[...t.map(g=>c(...g)),...l.default?[l.default()]:[]]);/**
+ * @license lucide-vue-next v0.562.0 - ISC
+ *
+ * This source code is licensed under the ISC license.
+ * See the LICENSE file in the root directory of this source tree.
+ */const C=(e,t)=>(r,{slots:o,attrs:s})=>c(y,{...s,...r,iconNode:t,name:e},o);export{C as c};
