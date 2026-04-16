5b953e037d68d49017f9d17161e1ba6aabaa04da
mvdbeek <mvdbeek@users.noreply.github.com>
2026-04-16 13:49:12 +0000

    deploy: 09b0f7eb1dd220c87ee56345a913abb21740312f

---
+++ _astro/chunk-FMBD7UC4.PdKRcdwK.js
@@ -0,0 +1,15 @@
+import{_ as o}from"./MarkdownRenderer.DaMhZxWg.js";(function(){try{var e=typeof window<"u"?window:typeof global<"u"?global:typeof globalThis<"u"?globalThis:typeof self<"u"?self:{};e.SENTRY_RELEASE={id:"09b0f7eb1dd220c87ee56345a913abb21740312f"};var n=new e.Error().stack;n&&(e._sentryDebugIds=e._sentryDebugIds||{},e._sentryDebugIds[n]="0557e113-ef40-4a33-910c-7deb304aa017",e._sentryDebugIdIdentifier="sentry-dbid-0557e113-ef40-4a33-910c-7deb304aa017")}catch{}})();var l=o(()=>`
+  /* Font Awesome icon styling - consolidated */
+  .label-icon {
+    display: inline-block;
+    height: 1em;
+    overflow: visible;
+    vertical-align: -0.125em;
+  }
+  
+  .node .label-icon path {
+    fill: currentColor;
+    stroke: revert;
+    stroke-width: revert;
+  }
+`,"getIconStyles");export{l as g};
