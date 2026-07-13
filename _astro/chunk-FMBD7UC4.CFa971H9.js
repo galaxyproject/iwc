eead910bb2713f52afa4d523875f844f4cd77b22
lldelisle <lldelisle@users.noreply.github.com>
2026-07-13 10:03:38 +0000

    deploy: 6dfe3616c2008097a9c5e477e8f2aae1d18eeb54

---
+++ _astro/chunk-FMBD7UC4.CFa971H9.js
@@ -0,0 +1,15 @@
+import{_ as o}from"./MarkdownRenderer.CAi-LI7M.js";(function(){try{var e=typeof window<"u"?window:typeof global<"u"?global:typeof globalThis<"u"?globalThis:typeof self<"u"?self:{};e.SENTRY_RELEASE={id:"6dfe3616c2008097a9c5e477e8f2aae1d18eeb54"};var n=new e.Error().stack;n&&(e._sentryDebugIds=e._sentryDebugIds||{},e._sentryDebugIds[n]="81bb1810-4b97-47c0-b047-95744cda14c9",e._sentryDebugIdIdentifier="sentry-dbid-81bb1810-4b97-47c0-b047-95744cda14c9")}catch{}})();var l=o(()=>`
+  /* Font Awesome icon styling - consolidated */
+  .label-icon {
+    display: inline-block;
+    height: 1em;
+    overflow: visible;
+    vertical-align: -0.125em;
+  }
+  
+  .node .label-icon path {
+    fill: currentColor;
+    stroke: revert;
+    stroke-width: revert;
+  }
+`,"getIconStyles");export{l as g};
