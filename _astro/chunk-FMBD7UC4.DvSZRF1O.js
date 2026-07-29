b359582acc41f6b4ef5b900808a185433da1cddb
dannon <dannon@users.noreply.github.com>
2026-07-29 23:43:13 +0000

    deploy: c51bcdfd10ceb1dd62ef35f3165ee3114d3ca858

---
+++ _astro/chunk-FMBD7UC4.DvSZRF1O.js
@@ -0,0 +1,15 @@
+(function(){try{var e=typeof window<`u`?window:typeof global<`u`?global:typeof globalThis<`u`?globalThis:typeof self<`u`?self:{};e.SENTRY_RELEASE={id:`c51bcdfd10ceb1dd62ef35f3165ee3114d3ca858`};var t=new e.Error().stack;t&&(e._sentryDebugIds=e._sentryDebugIds||{},e._sentryDebugIds[t]=`5fbe44f8-1d2c-4e7c-8802-e2a108b9900c`,e._sentryDebugIdIdentifier=`sentry-dbid-5fbe44f8-1d2c-4e7c-8802-e2a108b9900c`)}catch{}})();import{h as e}from"./src.C4d_3y0t.js";var t=e(()=>`
+  /* Font Awesome icon styling - consolidated */
+  .label-icon {
+    display: inline-block;
+    height: 1em;
+    overflow: visible;
+    vertical-align: -0.125em;
+  }
+  
+  .node .label-icon path {
+    fill: currentColor;
+    stroke: revert;
+    stroke-width: revert;
+  }
+`,`getIconStyles`);export{t};
