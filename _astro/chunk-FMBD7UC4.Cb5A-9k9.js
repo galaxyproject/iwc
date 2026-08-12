3a00de169df536e2105bc284c13c88540c717cbd
Delphine-L <Delphine-L@users.noreply.github.com>
2026-08-12 16:32:51 +0000

    deploy: 496fedc1e34c747851fa56724db42b10b2c80bd7

---
+++ _astro/chunk-FMBD7UC4.Cb5A-9k9.js
@@ -0,0 +1,15 @@
+(function(){try{var e=typeof window<`u`?window:typeof global<`u`?global:typeof globalThis<`u`?globalThis:typeof self<`u`?self:{};e.SENTRY_RELEASE={id:`496fedc1e34c747851fa56724db42b10b2c80bd7`};var t=new e.Error().stack;t&&(e._sentryDebugIds=e._sentryDebugIds||{},e._sentryDebugIds[t]=`5fbe44f8-1d2c-4e7c-8802-e2a108b9900c`,e._sentryDebugIdIdentifier=`sentry-dbid-5fbe44f8-1d2c-4e7c-8802-e2a108b9900c`)}catch{}})();import{h as e}from"./src.BgWdTefn.js";var t=e(()=>`
+  /* Font Awesome icon styling - consolidated */
+  .label-icon {
+    display: inline-block;
+    height: 1em;
+    overflow: visible;
+    vertical-align: -0.125em;
+  }
+  
+  .node .label-icon path {
+    fill: currentColor;
+    stroke: revert;
+    stroke-width: revert;
+  }
+`,`getIconStyles`);export{t};
