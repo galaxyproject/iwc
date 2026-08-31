07c9d4c174aea36bdc95d9dc4e68211243d80c13
RZ9082 <RZ9082@users.noreply.github.com>
2026-08-31 12:16:00 +0000

    deploy: 75e44efc571f71fbfdb909a2851a44c23c3af5b9

---
+++ _astro/chunk-FMBD7UC4.BIJJAfE4.js
@@ -0,0 +1,15 @@
+(function(){try{var e=typeof window<`u`?window:typeof global<`u`?global:typeof globalThis<`u`?globalThis:typeof self<`u`?self:{};e.SENTRY_RELEASE={id:`75e44efc571f71fbfdb909a2851a44c23c3af5b9`};var t=new e.Error().stack;t&&(e._sentryDebugIds=e._sentryDebugIds||{},e._sentryDebugIds[t]=`5fbe44f8-1d2c-4e7c-8802-e2a108b9900c`,e._sentryDebugIdIdentifier=`sentry-dbid-5fbe44f8-1d2c-4e7c-8802-e2a108b9900c`)}catch{}})();import{h as e}from"./src.C7IibACL.js";var t=e(()=>`
+  /* Font Awesome icon styling - consolidated */
+  .label-icon {
+    display: inline-block;
+    height: 1em;
+    overflow: visible;
+    vertical-align: -0.125em;
+  }
+  
+  .node .label-icon path {
+    fill: currentColor;
+    stroke: revert;
+    stroke-width: revert;
+  }
+`,`getIconStyles`);export{t};
